6333a7647e77ca284605296b23e1067bebf19063
VedantAnand17 <VEDANTANAND0077@gmail.com>
2025-06-01 23:23:31 +0530

    Implement code changes to enhance functionality and improve performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,33 +19,69 @@ export const metadata: Metadata = {
     template: `%s | ${DATA.name}`,
   },
   description: DATA.description,
+  keywords: [
+    "Vedant Anand",
+    "Full Stack Developer",
+    "Blockchain Developer",
+    "Web3 Developer",
+    "Next.js Developer",
+    "TypeScript Developer",
+    "React Developer",
+    "Portfolio",
+    // Add more relevant keywords
+  ],
+  authors: [{ name: DATA.name, url: "https://github.com/vedantanand17" }],
+  creator: DATA.name,
+  publisher: DATA.name,
+  alternates: {
+    canonical: DATA.url,
+  },
   openGraph: {
     title: `${DATA.name}`,
     description: DATA.description,
     url: DATA.url,
     siteName: `${DATA.name}`,
+    images: [
+      {
+        url: `${DATA.url}/og-image.jpg`,
+        width: 1200,
+        height: 630,
+        alt: `${DATA.name} Portfolio Preview`,
+      },
+    ],
     locale: "en_US",
     type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: `${DATA.name} - Full Stack Blockchain Developer`,
+    description: DATA.description,
+    site: "@vedantsx",
+    creator: "@vedantsx",
+    images: [`${DATA.url}/og-image.webp`],
   },
   robots: {
     index: true,
     follow: true,
+    nocache: false,
     googleBot: {
       index: true,
       follow: true,
+      noimageindex: false,
       "max-video-preview": -1,
       "max-image-preview": "large",
       "max-snippet": -1,
     },
   },
-  twitter: {
-    title: `${DATA.name}`,
-    card: "summary_large_image",
+  icons: {
+    icon: [{ url: `/favicon.ico` }],
   },
   verification: {
-    google: "",
+    google: "", // Add your Google verification code
     yandex: "",
   },
+  category: "Technology",
+  assets: [`${DATA.url}/public`],
 };
 
 export default function RootLayout({
