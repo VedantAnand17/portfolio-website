11e9a1a3e73c05991cda483d097fc8c6aa52c391
VedantAnand17 <VEDANTANAND0077@gmail.com>
2025-07-12 01:24:43 +0530

    feat: Update Next.js configuration and enhance tweet card image handling
    - Added remote patterns for Twitter image domains in next.config.mjs to support external image loading.
    - Replaced standard <img> tags with Next.js <Image> component in tweet-card.tsx for optimized image handling and added width and height attributes for better performance.

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,26 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'abs.twimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'video.twimg.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   compress: true,
   poweredByHeader: false,
