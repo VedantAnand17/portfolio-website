1177dd8ff3014cdda35317db8690595043c20d84
VedantAnand17 <VEDANTANAND0077@gmail.com>
2025-12-10 22:11:43 +0530

    lcp fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -128,6 +128,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link
+          rel="preload"
+          href="/vedantpfp.webp"
+          as="image"
+          fetchPriority="high"
+        />
         <GoogleAnalytics />
         <StructuredData />
         <WebsiteStructuredData />
